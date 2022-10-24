6620c2fa8085d7bde3b41bc88d8c35ac73ebd1a2
HyGamer59 <defossez.romain59@gmail.com>
2022-10-24 16:01:17 +0200

    Exercice 4

--- script.js
+++ script.js
@@ -7,4 +7,31 @@ var prenom = "romain"
 var âge = 23
 
 //Exercice 2 Opérations
+var pommes = 4
+var peches = 6
+var totalFruits = pommes * peches
+console.log(totalFruits)
 
+function sabrina(a, b, c, d) {
+    var res = a + b + c + d
+    return res
+}
+
+var sabrina1 = sabrina(25, 14, 2, 78)
+console.log(sabrina1)
+
+function cine() {
+    console.log("Ceci est une fonction")
+}
+cine()
+
+//Exercice 3 Les Objets
+const person ={
+    firstName:"Romain",
+    lastName:"Defossez",
+    villedenaissance:"Dechy"
+}
+alert(person.firstName)
+
+//Exercice 4 Variable
+ 
